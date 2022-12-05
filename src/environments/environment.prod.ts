6f5734729bbcdc49af5ac0a4fca0ffcd36039c88
Brian Wachanga <brian@klurdy.com>
2022-12-05 20:23:25 +0300

    add api key to build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  cohereApiKey: ''
+  cohereApiKey: '<API_KEY>'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  cohereApiKey: 'Np2HfHh0oADvg11H7y4PfNaaVBx7tQUD2nGRxEXP'
+  cohereApiKey: '<API_KEY>'
 };
 
 /*
